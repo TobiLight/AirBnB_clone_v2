0540d94ec0c0ad14c171291c2edf193858b44d88
TobiLight <jgetitdonefast@gmail.com>
2023-09-21 03:22:47 +0100

    Unittests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+""" Test doc"""
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """ Test doc"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Test doc"""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """ Test doc"""
         new = self.value()
         self.assertEqual(type(new.name), str)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,27 +8,27 @@ class test_User(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
-        """  """
+        """ Test doc """
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """  """
+        """ Test doc """
         new = self.value()
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """  """
+        """ Test doc """
         new = self.value()
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """  """
+        """ Test doc """
         new = self.value()
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """  """
+        """ Test doc """
         new = self.value()
         self.assertEqual(type(new.password), str)
