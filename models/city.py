72fb68889d89446d80b91889f76489d26df3f7d7
TobiLight <jgetitdonefast@gmail.com>
2023-09-21 02:25:23 +0100

    DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -9,13 +9,12 @@ import os
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    # name = Column(String(128), nullable=False)
-    # state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    # state = relationship('State', back_populates='cities')
+    
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        # state = relationship('State', backref='cities')
+        places = relationship('Place', cascade='all, delete, delete-orphan',
+                              backref='cities')
     else:
         name = ""
         state_id = ""
