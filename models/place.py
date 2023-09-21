a34ff080278f7811ff9e87a5eeaeefea572b158f
TobiLight <jgetitdonefast@gmail.com>
2023-09-21 03:00:59 +0100

    DBStorage - Amenity... and BOOM!

--- models/place.py
+++ models/place.py
@@ -1,21 +1,41 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 from models.review import Review
+from models.amenity import Amenity
 import os
+
+place_amenity = Table(
+    "place_amenity",
+    Base.metadata,
+    Column(
+        "place_id",
+        String(60),
+        ForeignKey("places.id"),
+        primary_key=True,
+        nullable=False,
+    ),
+    Column(
+        "amenity_id",
+        String(60),
+        ForeignKey("amenities.id"),
+        primary_key=True,
+        nullable=False,
+    ),
+)
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    
+
     if os.getenv("HBNB_TYPE_STORAGE") == 'db':
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        description =  Column(String(1024), nullable=True)
+        description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
@@ -24,6 +44,9 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=True)
         reviews = relationship('Review', cascade='all, delete, delete-orphan',
                                backref='place')
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
     else:
         city_id = ""
         user_id = ""
@@ -36,7 +59,7 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
-        
+
         @property
         def reviews(self):
             """Returns the list of Review instances with place_id equals to the
@@ -49,3 +72,23 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     result.append(review)
             return result
+
+        @property
+        def amenities(self):
+            """Returns the list of Amenity instances based on the attribute
+            amenity_ids"""
+            from models import storage
+            all_reviews = storage.all(Review)
+            result = []
+            # Check if review.id is same with place.id
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    result.append(review)
+            return result
+
+        @amenities.setter
+        def amenitites(self, amenity):
+            """Handles append method for adding an Amenity.id to the attribute
+            amenity_ids"""
+            if isinstance(amenity, Amenity):
+                self.amenity_ids.append(amenity.id)
