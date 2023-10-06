0091b9de4329d289c4d9fb483448d9dd4b30ff86
TobiLight <jgetitdonefast@gmail.com>
2023-10-06 03:06:46 +0100

    Creates and distributes an archive to your web servers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -46,7 +46,7 @@ def do_deploy(archive_path):
         # Upload the archive to /tmp/ directory on the web server
         archive_name = os.path.basename(archive_path.split("/")[-1].
                                         split('.')[0])
-       remote_path = "/tmp/"
+        remote_path = "/tmp/"
         put(archive_path, remote_path)
 
         # Create a new folder for the archive
