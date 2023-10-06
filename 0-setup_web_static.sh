1c63991b25c018236a8f96a79f90c1706c53e267
TobiLight <jgetitdonefast@gmail.com>
2023-10-06 03:22:27 +0100

    Sets up web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -37,14 +37,26 @@ if [ -f "$nginx_config" ]; then
     # Configure Nginx to serve the content using an alias
     echo "
 server {
-    listen 80;
+    listen 80 default_server;
     listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
     location /hbnb_static {
-        alias /data/web_static/current/;
+        alias /data/web_static/current;
+        index index.html index.htm;
     }
 
-    index index.html;
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
 }
 " | sudo tee "$nginx_config" > /dev/null
 
