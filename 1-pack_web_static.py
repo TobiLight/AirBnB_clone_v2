4892286ce278609ccf09e6513a2225eb61a6821c
TobiLight <jgetitdonefast@gmail.com>
2023-10-06 01:25:09 +0100

    Archive the contents of the web_static folder into a .tgz file.

---
+++ 1-pack_web_static.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# File: 1-pack_web_static.py
+# Author: Oluwatobiloba Light
+"""Generates a .tgz archive from the contents of the web_static folder"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """
+    Archive the contents of the web_static folder into a .tgz file.
+    """
+    dt = datetime.utcnow()
+    filename = "{}{}{}{}{}{}.tgz".format(
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+    try:
+        # Check if 'versions' directory exists; create it if not
+        if not os.path.isdir('versions'):
+            local('mkdir -p versions')
+
+        # Create the archive
+        file = local(
+            'tar -czvf versions/web_static_{} web_static'.format(filename))
+
+        # Check if the archive creation failed
+        if file.failed:
+            return None
+        return file
+
+    except Exception as e:
+        return None
+
+    # if os.path.isdir('versions') is False:
+    #     versions = local('mkdir -p versions')
+    #     if versions.failed is True:
+    #         return None
+    # file = local('tar -czvf versions/web_static_{}'.format(filename))
+    # if file.failed is True:
+    #     return None
+    # return file
