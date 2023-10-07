6c9827cb19ccf2e638eff1dc3feb2f80cd6ce74a
TobiLight <jgetitdonefast@gmail.com>
2023-10-07 02:01:33 +0100

    Fabric script to distribute an archive to your web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,13 +2,13 @@
 # File: 2-do_deploy_web_static.py
 # Author: Oluwatobiloba Light
 """Fabric script to distribute an archive to your web servers"""
-from fabric.api import run, put, run, env
+from fabric.api import run, put, run, env, task
 import os
 
 
 env.hosts = ['204.236.240.195', '34.239.253.9']
 
-
+@task
 def do_deploy(archive_path):
     """
     Distribute an archive to web servers.
@@ -26,27 +26,20 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
 
         run("sudo rm -rf {}{}/".format(dir_path, archive_name))
-
         # Create a new folder for the archive
         run('sudo mkdir -p {}{}/'.format(dir_path, archive_name))
-
         # Uncompress the archive to the new version directory
         run('sudo tar -xzf /tmp/{} -C {}{}/'.
             format(filename, dir_path, archive_name))
-
         # Delete the archive from the web server
         run('sudo rm /tmp/{}'.format(filename))
-
         # Move contents into the host web_static
         run('sudo mv {0}{1}/web_static/* {0}{1}/'.
             format(dir_path, archive_name))
-
         # Delete web_static compressed directory & files
         run('sudo rm -rf {}{}/web_static'.format(dir_path, archive_name))
-
         # Delete the symbolic link from the web server
         run('sudo rm -rf /data/web_static/current')
-#
         # # Create a new symbolic link
         run('sudo ln -sf {}{}/ \
             /data/web_static/current'.format(dir_path, archive_name))
