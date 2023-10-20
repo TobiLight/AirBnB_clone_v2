6991d3477a8c1aa1295355cae36145337ffa8035
TobiLight <jgetitdonefast@gmail.com>
2023-10-20 14:47:41 +0100

    Script that starts a Flask web application with multiple routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# File: 0-hbnb_route.py()
+# File: 1-hbnb_route.py()
 # Author: Oluwatobiloba Light
 """This script starts a Flask web application"""
 
@@ -14,7 +14,7 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb_():
+def hbnb():
     """Returns HBNB"""
     return "HBNB"
 
