fb71ba391953ae1a42c0852d338c4f5949b6dce3
TobiLight <jgetitdonefast@gmail.com>
2023-10-20 14:56:33 +0100

    Script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,10 +14,12 @@ def hello_hbnb():
     """Returns Hello HBNB!"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Returns HBNB"""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
