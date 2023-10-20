1f529e2a566cdf56967c2892f12dc65bd4b8b7eb
TobiLight <jgetitdonefast@gmail.com>
2023-10-20 14:56:13 +0100

    Script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# File: 2-c_route.py()
+# Author: Oluwatobiloba Light
+"""This script starts a Flask web application"""
+
+
+from flask import Flask
+app = Flask(__name__)
+from markupsafe import escape
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Display “C ” followed by the value of the text variable """
+    text = escape(text).replace("_", " ")
+    print("C is {}".format(text))
+    return f"C is {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
