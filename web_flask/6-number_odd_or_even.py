421c375686742873c62c24a03234d96d789e710e
TobiLight <jgetitdonefast@gmail.com>
2023-10-20 16:25:20 +0100

    Renders a HTML template only if n is an integer and if n is odd or even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -91,7 +91,8 @@ def number_odd_or_even(n):
                 _type_: _description_
     """
     odd_or_even = "even" if n % 2 == 0 else "odd"
-    return render_template('6-number_odd_or_even.html', n=n, odd_or_even=odd_or_even)
+    return render_template('6-number_odd_or_even.html', n=n,
+                           odd_or_even=odd_or_even)
 
 
 if __name__ == "__main__":
