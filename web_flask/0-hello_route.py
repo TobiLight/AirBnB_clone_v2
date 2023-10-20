98a8429e504bcf5ac081e37dcf9ef56839688cb7
TobiLight <jgetitdonefast@gmail.com>
2023-10-20 14:38:59 +0100

    Script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Returns Hello HBNB"""
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
