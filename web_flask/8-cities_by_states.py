f73b306c58c53fdc72066fccd61663ae0e231f8a
TobiLight <jgetitdonefast@gmail.com>
2023-10-22 21:52:20 +0100

    Flask route that renders html page that lists all states or a state by its id

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# File: 6-number_odd_or_even.py
+# File: 8-cities_by_state.py
 # Author: Oluwatobiloba Light
 """This script starts a Flask web application"""
 
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# File: 6-number_odd_or_even.py
+# File: 9-states.py
 # Author: Oluwatobiloba Light
 """This script starts a Flask web application"""
 
