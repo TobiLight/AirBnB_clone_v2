d80cacb06008bef6c0c0a040ca1e1c515035ceae
TobiLight <jgetitdonefast@gmail.com>
2023-10-22 11:48:57 +0100

    Fabric script that creates and distributes an archive to your web servers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -45,37 +45,60 @@ def do_deploy(archive_path):
     """
     Distribute an archive to web servers.
     """
+    # try:
+    #     if not os.path.exists(archive_path) or os.path.isfile(archive_path) \
+    #             is False:
+    #         return False
+
+    #     # Upload the archive to /tmp/ directory on the web server
+    #     filename = os.path.basename(archive_path.split("/")[-1])
+    #     archive_name = os.path.basename(filename.split('.')[0])
+    #     print("f-a",filename, archive_name)
+    #     fn_with_ext = os.path.basename(archive_path)
+    #     fn_no_ext, ext = os.path.splitext(fn_with_ext)
+    #     print('fff', fn_with_ext, fn_no_ext)
+    #     dir_path = "/data/web_static/releases/"
+
+    #     # Upload file to remote server
+    #     put(archive_path, "/tmp/")
+
+    #     run("sudo rm -rf {}{}/".format(dir_path, archive_name))
+    #     # Create a new folder for the archive
+    #     run('sudo mkdir -p {}{}/'.format(dir_path, archive_name))
+    #     # Uncompress the archive to the new version directory
+    #     run('sudo tar -xzf /tmp/{} -C {}{}/'.
+    #         format(filename, dir_path, archive_name))
+    #     # Delete the archive from the web server
+    #     run('sudo rm /tmp/{}'.format(filename))
+    #     # Move contents into the host web_static
+    #     run('sudo mv {0}{1}/web_static/* {0}{1}/'.
+    #         format(dir_path, archive_name))
+    #     # Delete web_static compressed directory & files
+    #     run('sudo rm -rf {}{}/web_static'.format(dir_path, archive_name))
+    #     # Delete the symbolic link from the web server
+    #     run('sudo rm -rf /data/web_static/current')
+    #     # # Create a new symbolic link
+    #     run('sudo ln -sf {}{}/ \
+    #         /data/web_static/current'.format(dir_path, archive_name))
+    #     print("New version deployed!")
+    #     return True
+    # except Exception:
+    #     return False
     try:
-        if not os.path.exists(archive_path) or os.path.isfile(archive_path) \
-                is False:
+        if not os.path.exists(archive_path):
             return False
-
-        # Upload the archive to /tmp/ directory on the web server
-        filename = os.path.basename(archive_path.split("/")[-1])
-        archive_name = os.path.basename(filename.split('.')[0])
-        dir_path = "/data/web_static/releases/"
-
-        # Upload file to remote server
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
-
-        run("sudo rm -rf {}{}/".format(dir_path, archive_name))
-        # Create a new folder for the archive
-        run('sudo mkdir -p {}{}/'.format(dir_path, archive_name))
-        # Uncompress the archive to the new version directory
-        run('sudo tar -xzf /tmp/{} -C {}{}/'.
-            format(filename, dir_path, archive_name))
-        # Delete the archive from the web server
-        run('sudo rm /tmp/{}'.format(filename))
-        # Move contents into the host web_static
-        run('sudo mv {0}{1}/web_static/* {0}{1}/'.
-            format(dir_path, archive_name))
-        # Delete web_static compressed directory & files
-        run('sudo rm -rf {}{}/web_static'.format(dir_path, archive_name))
-        # Delete the symbolic link from the web server
-        run('sudo rm -rf /data/web_static/current')
-        # # Create a new symbolic link
-        run('sudo ln -sf {}{}/ \
-            /data/web_static/current'.format(dir_path, archive_name))
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
         print("New version deployed!")
         return True
     except Exception:
