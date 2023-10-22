4637b7326c6879fb65bc90d557fe4a8b5b4cc2e6
TobiLight <jgetitdonefast@gmail.com>
2023-10-22 14:06:55 +0100

    Fabric script that creates and distributes an archive to your web servers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -35,13 +35,6 @@ def do_pack():
         return filename
     except Exception as e:
         return None
-    # formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
-    # mkdir = "mkdir -p versions"
-    # path = "versions/web_static_{}.tgz".format(formatted_dt)
-    # print("Packing web_static to {}".format(path))
-    # if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
-    #     return path
-    # return None
 
 
 @task
